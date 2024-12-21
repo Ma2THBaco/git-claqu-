0dd50fa342591bfcb5fb44fa503d0f2285d9de31
Ma2THBaco <debailliencourtmatthieu@gmail.com>
2024-12-21 10:18:14 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function App(){
   return (
-<View style={{flex: 1, backgroundColor:'green'}}>
+<View style={{flex: 1, backgroundColor:'blue'}}>
     
 </View>
 
